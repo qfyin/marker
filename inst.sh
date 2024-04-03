791968055b047b692529b76ff2df44290bbe7192
Qiufeng <qfyin@hotmail.com>
2024-04-03 20:01:27 +0800

    update script

--- inst.sh
+++ inst.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 DEBIAN_FRONTEND=noninteractive
 
 # Install required packages
-apt-get install -y curl wget git make lsb-release gcc
+sudo apt-get install -y curl wget git make lsb-release gcc
 
 # 1. Install poetry
 curl -sSL https://install.python-poetry.org | python3 -
@@ -22,8 +21,8 @@ cd marker
 chmod +x ./scripts/install/tesseract_5_install.sh
 ./scripts/install/tesseract_5_install.sh
 chmod +x ./scripts/install/ghostscript_install.sh
-./scripts/install/ghostscript_install.sh
-cat scripts/install/apt-requirements.txt | xargs apt-get install -y
+sudo ./scripts/install/ghostscript_install.sh
+cat scripts/install/apt-requirements.txt | xargs sudo apt-get install -y
 
 # 5. Create local.env file
 # it's already in repo
@@ -38,3 +37,7 @@ poetry install
 # 8. Update pytorch
 poetry remove torch
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# for test
+# wget https://arxiv.org/pdf/2304.04487.pdf
+# python3 s
