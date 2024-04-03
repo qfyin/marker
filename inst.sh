57033e9d3fa04d13e8b89ff77f38bdae95da904f
Qiufeng <qfyin@hotmail.com>
2024-04-03 16:39:54 +0800

    add install script

---
+++ inst.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+DEBIAN_FRONTEND=noninteractive
+
+# Install required packages
+apt-get install -y curl wget git make lsb-release gcc
+
+# 1. Install poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# 2. Add `export PATH="$HOME/.local/bin:$PATH"` to your shell configuration file.
+export PATH="$HOME/.local/bin:$PATH"
+
+# 3. Clone code from Github
+git clone https://github.com/qfyin/marker.git
+cd marker
+
+# 4. Install libraries
+chmod +x ./scripts/install/tesseract_5_install.sh
+./scripts/install/tesseract_5_install.sh
+chmod +x ./scripts/install/ghostscript_install.sh
+./scripts/install/ghostscript_install.sh
+cat scripts/install/apt-requirements.txt | xargs apt-get install -y
+
+# 5. Create local.env file
+# it's already in repo
+
+# 6. Create conda environment
+# no need
+
+# 7. Install python requirements
+poetry lock
+poetry install
+
+# 8. Update pytorch
+poetry remove torch
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
